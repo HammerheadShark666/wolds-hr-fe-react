bff4b7a638848a08a8a5a52d0e902769724f1276
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2025-06-30 17:44:59 +0100

    Change layout/sidebar for responsive design

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,19 +1,46 @@
-// import Header from './Header';  
+// import styles from "./css/Layout.module.css";
+// import { useState } from 'react'; 
+// import { Outlet } from 'react-router-dom';
+// import Sidebar from './Sidebar';
+// import Header from "./Header";
+
+// const Layout = () => {
+//   const [sidebarOpen, setSidebarOpen] = useState(false);
+  
+//   return (
+//     <div className={styles["layout"]}>
+//       <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+//         {/* <Sidebar></Sidebar> */}
+//       <div className={styles["main-content"]}>
+//         <Header onHamburgerClick={() => setSidebarOpen(true)} />
+//         <div className={styles["content"]}>
+//           <div className={styles["card"]}>    
+//             <div className={styles["card-content"]}>
+//               <Outlet /> 
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Layout;
+
+import Header from './Header';  
 import styles from "./css/Layout.module.css";
-// import Sidebar from "./Sidebar"; 
-//import { useState } from 'react'; 
+import Sidebar from "./Sidebar"; 
+import { useState } from 'react'; 
 import { Outlet } from 'react-router-dom';
-import Sidebar from './Sidebar';
 
 const Layout = () => {
-  // const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   
   return (
     <div className={styles["layout"]}>
-      {/* <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} /> */}
-        <Sidebar></Sidebar>
+      <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
       <div className={styles["main-content"]}>
-        {/* <Header onHamburgerClick={() => setSidebarOpen(true)} /> */}
+        <Header onHamburgerClick={() => setSidebarOpen(true)} />
         <div className={styles["content"]}>
           <div className={styles["card"]}>    
             <div className={styles["card-content"]}>
